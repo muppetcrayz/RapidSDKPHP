4b54098d0b55fc6cf3efbed0e098c992b0c3f234
Sage Conger <sconger@capital.edu>
2018-07-16 14:13:38 -0400

    fixing post issues

--- login.php
+++ login.php
@@ -2,38 +2,41 @@
 session_start();
 $result = '';
 $url = 'https://api.rapidsdk.com/v1/login';
-$data = array('username' => $_POST['username'], 'password' => $_POST['password']);
 
-$apiKey = "4abc7598e1f28e394d57f50396c92a160671b575776ed10d885281eb94db7259";
-$apiSecret = "eb1f101fe943b41526ae1c5e54089834badbab836e87f73ee1b6047e30a41c68";
-$tokenstr = $apiKey . ":" . $apiSecret;
-$token = base64_encode($tokenstr);
+if (isset($_POST['username']) && isset($_POST['password'])) {
+  $data = array('username' => $_POST['username'], 'password' => $_POST['password']);
 
-$_SESSION['token'] = $token;
+  $apiKey = "4abc7598e1f28e394d57f50396c92a160671b575776ed10d885281eb94db7259";
+  $apiSecret = "eb1f101fe943b41526ae1c5e54089834badbab836e87f73ee1b6047e30a41c68";
+  $tokenstr = $apiKey . ":" . $apiSecret;
+  $token = base64_encode($tokenstr);
 
-$ch = curl_init();
-$headers[] = "Authorization: Basic " . $_SESSION['token'];
+  $_SESSION['token'] = $token;
 
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HTTPHEADER,  $headers);
-curl_setopt($ch, CURLOPT_POST, TRUE);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$content = json_decode(curl_exec($ch), true);
+  $ch = curl_init();
+  $headers[] = "Authorization: Basic " . $_SESSION['token'];
+
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_HTTPHEADER,  $headers);
+  curl_setopt($ch, CURLOPT_POST, TRUE);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $content = json_decode(curl_exec($ch), true);
 
 
-if ($content['status'] == "Success") {
-  $_SESSION['session_id'] = $content['session_id'];
-  header('location:data.php');
+  if ($content['status'] == "Success") {
+    $_SESSION['session_id'] = $content['session_id'];
+    header('location:data.php');
+  }
+  else if ($content['status'] == "Failed"){
+    print_r("Incorrect username or password.");
+  }
+
+  curl_close($ch);
+
 }
-else if ($content['status'] == "Failed"){
-  print_r("Incorrect username or password.");
-}
-
-curl_close($ch);
-
 ?>
 
 <!DOCTYPE html>
--- signup.php
+++ signup.php
@@ -1,28 +1,30 @@
 <?php
 $result = '';
 $url = 'https://api.rapidsdk.com/v1/register';
-$data = array('firstname' => $_POST['firstname'], 'lastname' => $_POST['lastname'],
-'email' => $_POST['email'], 'password' => $_POST['password']);
 
-$ch = curl_init();
-$headers[] = "Authorization: Basic " . $token;
+if (isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['password'])) {
+  $data = array('firstname' => $_POST['firstname'], 'lastname' => $_POST['lastname'],
+  'email' => $_POST['email'], 'password' => $_POST['password']);
 
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HTTPHEADER,  $headers);
-curl_setopt($ch, CURLOPT_POST, TRUE);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$content = json_decode(curl_exec($ch), true);
+  $ch = curl_init();
+  $headers[] = "Authorization: Basic " . $token;
 
-if ($content['status'] == "Success") {
-  header('location:login.php');
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_HTTPHEADER,  $headers);
+  curl_setopt($ch, CURLOPT_POST, TRUE);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $content = json_decode(curl_exec($ch), true);
+
+  if ($content['status'] == "Success") {
+    header('location:login.php');
+  }
+  else if ($content['status'] == "Failed"){
+    print_r("Please try again.");
+  }
 }
-else if ($content['status'] == "Failed"){
-  print_r("Please try again.");
-}
-
 ?>
 
 <!DOCTYPE html>
